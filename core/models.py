129f0516ab31455ce11fefe0371b3910b45dadb5
charliewu <charliewu@rayark.com>
2023-02-17 15:24:52 +0800

    add super user

--- core/models.py
+++ core/models.py
@@ -1,8 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, UserManager, PermissionsMixin
 
-class User(AbstractBaseUser):
-    email = models.EmailField(unique=True)
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=20, unique=True)
+    email = models.EmailField()
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
-    USERNAME_FIELD = 'email'
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = []
