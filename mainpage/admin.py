722c8355fbf6c3ccb36b73893944bff3b4c0215c
charliewu <charliewu@rayark.com>
2023-02-22 09:39:30 +0800

    add player to admin

--- mainpage/admin.py
+++ mainpage/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.http import urlencode
+from django.urls import reverse
+from . import models
 
 # Register your models here.
+
+@admin.register(models.Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ['nick_name', 'coin', 'user_is_superuser', 'user_is_staff', 'user_link']
+    list_editable = ['coin']
+    list_select_related = ['user']
+
+    def user_is_superuser(self, player: models.Player):
+        return player.user.is_superuser
+
+    def user_is_staff(self, player: models.Player):
+        return player.user.is_staff
+
+    def user_link(self, player: models.Player):
+        url = reverse('admin:core_user_changelist') + '?' + urlencode({'player__user__id': player.user.id})
+        return format_html('<a href="{}">{}</a>', url, player.user.username)
--- mainpage/models.py
+++ mainpage/models.py
@@ -4,4 +4,11 @@ from django.conf import settings
 # Create your models here.
 class Player(models.Model):
     nick_name = models.CharField(max_length=20)
+    coin = models.IntegerField(default=1000)
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, primary_key=True)
+
+    def __str__(self) -> str:
+        return self.nick_name
+
+    class Meta:
+        ordering = ['nick_name']
