f1693915d993cb43854eb9f5b2e664490efef134
charliewu <charliewu@rayark.com>
2023-02-26 17:45:02 +0800

    coin readonly

--- mainpage/serializers.py
+++ mainpage/serializers.py
@@ -3,7 +3,8 @@ from core.serializers import UserSerializer
 from .models import Player
 
 class PlayerSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
+    user = UserSerializer(read_only=True)
+    coin = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Player
