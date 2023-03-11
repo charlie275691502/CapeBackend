9e3bc0c8a450f27b7a4c3e3cae201edbb7745a35
charliewu <charliewu@rayark.com>
2023-03-11 12:31:33 +0800

    change to synchronize consumer

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,21 +1,27 @@
 import json
-from channels.generic.websocket import AsyncWebsocketConsumer
-from channels.db import database_sync_to_async
+from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 from .models import Message, Room
 from .serializers import MessageSerializer, RoomPlayerSerializer
 
 
-class ChatConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
         room_id = self.scope["url_route"]["kwargs"]["room_id"]
         self.room_group_name = "chat_%s" % room_id
-        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-        await self.accept()
 
-    async def disconnect(self, close_code):
-        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, self.channel_name
+        )
 
-    async def receive(self, text_data):
+        self.accept()
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
+
+    def receive(self, text_data):
         text_data_json = json.loads(text_data)
         room_id = self.scope["url_route"]["kwargs"]['room_id']
         player_id = self.scope["user"].id
@@ -23,36 +29,70 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         if command == "send_message" :
             content = text_data_json["content"]
-            data = await self.create_message(room_id, player_id, content)
-        
-            await self.channel_layer.group_send(
-                self.room_group_name, {"type": "send_message", "command": "send_message", "data": data}
-            )
-        elif command == "join_room" :
-            data = await self.try_join_room(room_id, player_id)
-        
-            await self.channel_layer.group_send(
-                self.room_group_name, {"type": "update_room_players", "command": "update_room_players", "data": data}
+            data = self.create_message(room_id, player_id, content)
+            
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name, {"type": "send_data", "command": "send_message", "data": data}
             )
 
-    async def send_message(self, event):
+    def send_data(self, event):
         data = event["data"]
         command = event["command"]
-        await self.send(text_data=json.dumps({"command": command, "data": data}))
+        self.send(text_data=json.dumps({"command": command, "data": data}))
 
-    async def update_room_players(self, event):
-        data = event["data"]
-        command = event["command"]
-        await self.send(text_data=json.dumps({"command": command, "data": data}))
-
-    @database_sync_to_async
     def create_message(self, room_id, player_id, content):
         message = Message.objects.create(room_id=room_id, player_id=player_id, content=content)
         serializer = MessageSerializer(message)
         return serializer.data
     
-    @database_sync_to_async
+class RoomManageConsumer(WebsocketConsumer):
+    def connect(self):
+        self.room_group_name = "chat_room_manage"
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, self.channel_name
+        )
+
+        self.accept()
+
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
+
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        player_id = self.scope["user"].id
+        command = text_data_json["command"]
+        room_id = text_data_json['room_id']
+        
+        if command == "join_room" :
+            data = self.try_join_room(room_id, player_id)
+            
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name, {"type": "send_data", "command": "update_room_players", "data": data}
+            )
+        elif command == "leave_room" :
+            data = self.try_join_room(room_id, player_id)
+            
+            async_to_sync(self.channel_layer.group_send)(
+                self.room_group_name, {"type": "send_data", "command": "update_room_players", "data": data}
+            )
+    
     def try_join_room(self, room_id, player_id):
         room = Room.objects.filter(id=room_id).first()
+        # add player to room
         serializer = RoomPlayerSerializer(room)
         return serializer.data
+    
+    def leave_room(self, room_id, player_id):
+        room = Room.objects.filter(id=room_id).first()
+        # remove player from room
+        serializer = RoomPlayerSerializer(room)
+        return serializer.data
+
+    def send_data(self, event):
+        data = event["data"]
+        command = event["command"]
+        self.send(text_data=json.dumps({"command": command, "data": data}))
+
--- chat/routing.py
+++ chat/routing.py
@@ -1,7 +1,8 @@
 from django.urls import re_path
 
-from . import consumers
+from .consumers import ChatConsumer, RoomManageConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/rooms/(?P<room_id>\w+)/$", consumers.ChatConsumer.as_asgi()),
+    re_path(r"ws/chat/rooms/(?P<room_id>\w+)/$", ChatConsumer.as_asgi()),
+    re_path(r"ws/chat/room_manage/$", RoomManageConsumer.as_asgi()),
 ]
