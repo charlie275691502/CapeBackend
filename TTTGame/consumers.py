9f22d40de09650e61ecf1a52cc6b95c65c07d429
charliewu <charliewu@rayark.com>
2023-12-18 16:40:08 +0800

    choose position action logic

--- TTTGame/consumers.py
+++ TTTGame/consumers.py
@@ -2,8 +2,15 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
 from channels.db import database_sync_to_async
+from .models import TTTAction, TTTChoosePositionActionCommand
+from .serializer import TTTActionSerialier
 
 class TTTGameConsumer(AsyncWebsocketConsumer):
+    def __init__(self):
+        game_id = self.scope["url_route"]["kwargs"]["game_id"]
+        self.game = await database_sync_to_async(self.get_board) (
+                game_id)
+
     async def connect(self):
         game_id = self.scope["url_route"]["kwargs"]["game_id"]
         self.game_group_name = "game_%s" % game_id
@@ -15,12 +22,6 @@ class TTTGameConsumer(AsyncWebsocketConsumer):
         await self.accept()
 
     async def disconnect(self, close_code):
-        game_id = self.scope["url_route"]["kwargs"]["game_id"]
-        player_id = self.scope["user"].id
-        await database_sync_to_async(self.try_leave_room)(
-            game_id,
-            player_id)
-
         await self.channel_layer.group_discard(
             self.game_group_name,
             self.channel_name)
@@ -30,6 +31,36 @@ class TTTGameConsumer(AsyncWebsocketConsumer):
         player_id = self.scope["user"].id
         game_id = self.scope["url_route"]["kwargs"]['game_id']
         command = text_data_json["command"]
+
+        if is_player_turn(player_id) == False:
+            await self.send_command_fail(command, "Not your turn yet.")
+            return
+
+        if command == "choose_position_action" :
+            position = text_data_json["position"]
+
+            if is_position_valid(position) == False:
+                await self.send_command_fail(command, "Position invalid")
+                return
+
+            data = await database_sync_to_async(self.create_choose_position_action) (
+                game_id,
+                player_id,
+                position)
+
+            await database_sync_to_async(self.execute_choose_position_action) (
+                game_id,
+                player_id,
+                position)
+
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    "type": "send_data",
+                    "command": "choose_position_action",
+                    "data": data
+                })
+            await self.send_command_success(command)
     
     async def send_command_success(self, command):
         await self.send(text_data=json.dumps({"command": f"{command}_success", "data": ""}))
@@ -41,3 +72,34 @@ class TTTGameConsumer(AsyncWebsocketConsumer):
         command = event["command"]
         data = event["data"]
         await self.send(text_data=json.dumps({"command": command, "data": data}))
+
+    def get_board(self, game_id):
+        return TTTGame.objects.filter(id=game_id).first()
+
+    def is_player_turn(self, player_id):
+        player = self.game.player_set.players.filter(id=player_id).first()
+        return self.game.board.turn_of_team == player.team
+
+    def is_position_valid(self, position):
+        return 
+            0 <= position and
+            position < self.game.setting.board_size ** 2 and
+            self.game.board.positions[position] == 0
+
+    def create_choose_position_action(self, game_id, player_id, position):
+        player = self.game.player_set.players.filter(id=player_id).first()
+
+        action_command = TTTChoosePositionActionCommand.objects.create(position=position)
+        action = TTTAction.objects.create(
+            action_set_id=self.game.player_set.action_set.id,
+            player_id=player.player.user.id,
+            action_command=action_command)
+
+        serializer = TTTActionSerialier(action)
+        return serializer.data
+
+    def execute_choose_position_action(self, game_id, player_id, position):
+        player = self.game.player_set.players.filter(id=player_id).first()
+        team = player.team
+        self.game.board.position[position] = team
+        self.game.board.save()
