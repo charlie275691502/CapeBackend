bd48140ee2c5614b74713582002f6b70c7d28231
charlie275691502 <charlie275691502@gmail.com>
2024-06-03 01:11:15 +1000

    fix daphne register

--- capebackend/asgi.py
+++ capebackend/asgi.py
@@ -1,14 +1,17 @@
 import os
 
-from channels.auth import AuthMiddlewareStack
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "capebackend.settings.dev")
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+
+import django
+django.setup()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from .channelsmiddleware import JwtAuthMiddlewareStack
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "capebackend.settings.dev")
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
 import chat.routing, TTTGame.routing
--- capebackend/settings/common.py
+++ capebackend/settings/common.py
@@ -30,7 +30,6 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'daphne',
     'channels',
-    'daphne',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
